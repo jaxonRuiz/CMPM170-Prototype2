0079fda9114fb09e6fe58d199bace80542f7df19
eltonzeng <126036056+eltonzeng@users.noreply.github.com>
2024-11-13 01:49:07 -0800

    popup window logic
    With slider and button to open window (pairs with window.tscn for testing)

---
+++ Scripts/window.gd
@@ -0,0 +1,15 @@
+extends Camera2D
+
+func _on_window_close_requested() -> void: $Window.hide()
+
+
+func _on_medicines_slide_value_changed(value: float) -> void:
+	print("Medicines level: %d\n" % value)
+	
+
+
+
+func _on_button_pressed() -> void:
+	if not $Window.is_visible():
+		$Window.show()
+		$Window.grab_focus()  
