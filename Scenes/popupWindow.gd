0b891a83a1ccd1327a7f3ad9af29ee40521df24c
eltonzeng <126036056+eltonzeng@users.noreply.github.com>
2024-11-13 18:26:19 -0800

    populated functions in popup window script

--- Scenes/popupWindow.gd
+++ Scenes/popupWindow.gd
@@ -1,6 +1,11 @@
 extends Window
 
-
+@onready var med_slide = $Control/Panel/Medicine/MedicinesSlide
+@onready var mat_slide = $Control/Panel/Materials/MaterialsSlide
+@onready var know_slide = $Control/Panel/Knowledge/KnowledgeSlide
+@onready var secu_slide = $Control/Panel/Security/SecuritySlide
+@onready var food_slide = $Control/Panel/Food/FoodSlide
+@onready var tools_slide = $Control/Panel/Tools/ToolsSlide
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,18 +18,63 @@ func _process(delta: float) -> void:
 
 
 func setCurrentValues(resources:Dictionary):
-	$Control/Panel/Medicine/MedicinesSlide.value = resources["medicine"]
-	pass
+	if resources.has("medicine"):
+		med_slide.value = resources["medicine"]
+	else:
+		med_slide.value = 0
+	if resources.has("tools"):
+		tools_slide.value = resources["tools"]
+	else:
+		tools_slide.value = 0
+	if resources.has("knowledge"):
+		know_slide.value = resources["knowledge"]
+	else:
+		know_slide.value = 0
+	if resources.has("security"):
+		secu_slide.value = resources["security"]
+	else:
+		secu_slide.value = 0
+	if resources.has("materials"):
+		mat_slide.value = resources["materials"]
+	else:
+		mat_slide.value = 0
+	if resources.has("food"):
+		food_slide.value = resources["food"]
+	else:
+		food_slide.value = 0
+	
 	
 func setMaxValues(resources:Dictionary):
-	$Control/Panel/Medicine/MedicinesSlide.max_value = resources["medicine"]
-	pass;
+	if resources.has("medicine"):
+		med_slide.max_value = resources["medicine"]
+	else:
+		med_slide.max_value = 0
+	if resources.has("tools"):
+		tools_slide.max_value = resources["tools"]
+	else:
+		tools_slide.max_value = 0
+	if resources.has("food"):
+		food_slide.max_value = resources["food"]
+	else:
+		food_slide.max_value = 0
+	if resources.has("materials"):
+		mat_slide.max_value = resources["materials"]
+	else:
+		mat_slide.max_value = 0
+	if resources.has("security"):
+		sec_slide.max_value = resources["security"]
+	else:
+		sec_slide.max_value = 0
+	if resources.has("knowledge"):
+		know_slide.max_value = resources["knowledge"]
+	else:
+		know_slide.max_value = 0
 
 
 func getResourceValues():
 	var resources = {};
-	resources["medicine"] = $Control/Panel/Medicine/MedicinesSlide.value;
-	resources["food"] = $Control/Panel/Food/FoodSlide.value
+	resources["medicine"] = med_slide.value;
+	resources["food"] = food_slide.value
 	
 	print(resources);
 	return resources;
